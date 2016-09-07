dd5b6f484d35e0b310170695b797664c6cbdf987
Maria Famador <maria.famador87@gmail.com>
2016-09-07 16:41:51 +0800

    Setup for IndexRoute type

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -2,14 +2,18 @@ import React from "react";
 import {Link} from "react-router";
 
 export default class Layout extends React.Component {
+  navigate() {
+    this.props.history.pushState(null, '/');
+  }
+
   render() {
     return (
       <div>
         <h1>KillerNews.net</h1>
         {this.props.children}
         <Link to="archives" class='btn btn-danger'>archives</Link>
-        <Link to="featured" class='btn btn-warning'>featured</Link>
         <Link to="settings" class='btn btn-success'>settings</Link>
+        <button class='btn btn-warning' onClick={this.navigate.bind(this)}>featured</button>
       </div>
     );
   }
