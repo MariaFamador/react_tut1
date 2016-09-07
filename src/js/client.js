eff2951589f76682d8dcdf2ba19eff8c52a69968
Maria Famador <maria.famador87@gmail.com>
2016-09-07 16:32:22 +0800

    Add initial routes for pages

--- src/js/client.js
+++ src/js/client.js
@@ -2,7 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
+import Archives from "./pages/Archives";
+import Featured from "./pages/Featured";
 import Layout from "./pages/Layout";
+import Settings from "./pages/Settings";
+
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path='/' component={Layout}>
+      <IndexRoute component={Featured}></IndexRoute>
+      <Route path='archives' component={Archives}></Route>
+      <Route path='settings' component={Settings}></Route>
+    </Route>
+  </Router>, 
+app); 
--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,9 +1,16 @@
 import React from "react";
+import {Link} from "react-router";
 
 export default class Layout extends React.Component {
   render() {
     return (
-      <h1>KillerNews.net</h1>
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to="archives" class='btn btn-danger'>archives</Link>
+        <Link to="featured" class='btn btn-warning'>featured</Link>
+        <Link to="settings" class='btn btn-success'>settings</Link>
+      </div>
     );
   }
 }
